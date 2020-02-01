3f532960839be2898e9ef4ab9966d925220a795c
Silvio Limeira <silvio.limeira@live.com>
2020-02-01 16:10:30 -0300

    Fundamental Data Types

--- main.rs
+++ main.rs
@@ -1,3 +1,45 @@
+use std::mem;
+
 fn main() {
-    println!("Hello, world!")
+    // Fundamental Data Types
+    let a:u8 = 123;
+    println!("a = {}", a);
+
+    // a = 456;
+
+    // mutable
+    let mut b:i8 = 0; // mutable
+
+    println!("b = {}", b);
+    b = 42;
+    println!("b = {}", b);
+
+    let mut c = 123456789;  // 32-bit signed 32 i32
+    println!("c = {}, size = {} bytes", c, mem::size_of_val(&c));
+    c = -1;
+    println!("c = {}, size = {} bytes", c, mem::size_of_val(&c));
+
+    // i8 u8 i16 u16 i32 u32 i64 u64
+    let z:isize = 123; // izeze/usize
+    let size_of_z = mem::size_of_val(&z);
+    println!("z = {}, takes up {} bytes, {}-bit OS", 
+        z, size_of_z, size_of_z * 8);
+
+    let d:char = 'x';
+    println!("d = {}, size = {} bytes", d, mem::size_of_val(&d));
+
+    // raw numbers
+
+    let e = 2.5;
+    println!("e = {}, size = {} bytes", e, mem::size_of_val(&e));
+    let e1:f32 = 2.5;  // double-precision, 8 bytes or 64 bits, f64
+    println!("e = {}, size = {} bytes", e1, mem::size_of_val(&e1));
+
+    // true false
+    let g = false;
+    println!("g = {}, size = {} bytes", g, mem::size_of_val(&g));
+    let f = 4>0; // true
+    println!("f = {}, size = {} bytes", f, mem::size_of_val(&f));
+
+
 }
