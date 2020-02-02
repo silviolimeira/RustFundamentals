f0024fa4d2ff97e6769d4f725ef5c066c9dd6601
Silvio Limeira <silvio.limeira@live.com>
2020-02-02 11:39:45 -0300

    While and Loop

---
+++ WhileLoop/main.rs
@@ -0,0 +1,26 @@
+fn while_and_loop()
+{
+    let mut x = 1;
+
+    while x < 100 {
+        x *= 2;
+
+        if x == 64 {
+            println!("*64*");
+            continue;
+        }
+        println!("x = {}", x);
+    }
+
+    let mut y = 1;
+    loop { // while true
+        y *= 2;
+        println!("y = {}", y);
+
+        if y == 1<<10 { break; }
+    }
+}
+
+fn main() {
+    while_and_loop();
+}
